4f38f81bc0d7296f3ae1942dc9075fbd419fdf47
Edward <xhuddie@gmail.com>
2018-11-05 19:12:40 +0800

    Create iniparser.sh
    Reading function only, now

---
+++ parser/ini/iniparser.sh
@@ -0,0 +1,21 @@
+
+declare -A conf;
+read-ini(){
+	confile=$1
+	while read line
+	do
+		if [[ $line =~ ^[:blank:]*[^\;] ]]; then
+			if [[ $line =~ \[.*\] ]]; then
+				line=${line#\[};
+				line=${line%\]};
+				cur=$line-_-;
+			else
+				key=`echo $line | cut -d '=' -f 1`;
+				key=${key% };
+				val=`echo $line | cut -d '=' -f 2`;
+				val=${val# };
+				conf["$cur""$key"]="$val";
+			fi
+		fi
+	done < $confile
+}
