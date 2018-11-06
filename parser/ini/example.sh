91e85d1c8ded7c014933c992eacda731d7a7c808
Edward <xhuddie@gmail.com>
2018-11-06 11:39:09 +0800

    Update example.sh
    example of write function added

--- parser/ini/example.sh
+++ parser/ini/example.sh
@@ -2,6 +2,9 @@
 
 source iniparser.sh
 
-read-ini /etc/ceph/ceph.conf;
+read-ini /etc/ceph/test.conf;
 result=$(get-cfg global "enable experimental unrecoverable data corrupting features");
 echo $result;
+
+put-cfg global "enable experimental unrecoverable data corrupting features" "bluestore testval";
+write-ini /etc/ceph/test.conf;
